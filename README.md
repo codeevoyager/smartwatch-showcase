d92d5af02f6db9e556e9371feeafd5ee43bcff42
codeevoyager <vinothfromearth@gmail.com>
2023-09-22 21:08:34 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Smartwatch showcase
+
+A simple smartwatch showcase landing page created using HTML, CSS, Javascript.
+
+## Preview
+
+![preview-image](./preview-img.png)
---
+++ main.js
@@ -0,0 +1,21 @@
+
+//colors
+function black(){
+    document.getElementById("preview-image").src = "./watches/black.png";
+}
+
+function blue(){
+    document.getElementById("preview-image").src = "./watches/blue.png";
+}
+
+function pink(){
+    document.getElementById("preview-image").src = "./watches/pink.png";
+}
+
+function purple(){
+    document.getElementById("preview-image").src = "./watches/purple.png";
+}
+
+function red(){
+    document.getElementById("preview-image").src = "./watches/red.png";
+}
